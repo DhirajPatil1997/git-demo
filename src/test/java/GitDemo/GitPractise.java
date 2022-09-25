e70d9c5365252db1a8c4bcbcf748dd85bb0cb812
Dhiraj <dhiraj.patilcsn1@gmail.com>
2022-09-25 14:23:35 +0530

    first commit

---
+++ src/test/java/GitDemo/GitPractise.java
@@ -0,0 +1,12 @@
+package GitDemo;
+
+public class GitPractise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First line is added");
+		System.out.println("second line is added");
+		System.out.println("third line is added");
+	}
+
+}
