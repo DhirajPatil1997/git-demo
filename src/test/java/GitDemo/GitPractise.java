ae7bd7ee964982f7b00c4cc168c9be2081bf40ae
Dhiraj <dhiraj.patilcsn1@gmail.com>
2022-09-25 14:59:57 +0530

    pratice is updated

--- src/test/java/GitDemo/GitPractise.java
+++ src/test/java/GitDemo/GitPractise.java
@@ -5,8 +5,6 @@ public class GitPractise {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First line is added");
-		System.out.println("second line is added");
-		System.out.println("third line is added");
 	}
 
 }
