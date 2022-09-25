a1584a3e0cd89e467e2eadc66eac6c8f01bc9a22
DhirajPatil1997 <114327664+DhirajPatil1997@users.noreply.github.com>
2022-09-25 15:04:18 +0530

    Update GitPractise.java

--- src/test/java/GitDemo/GitPractise.java
+++ src/test/java/GitDemo/GitPractise.java
@@ -5,6 +5,10 @@ public class GitPractise {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First line is added");
+		System.out.println("First line is added");
+		System.out.println("First line is added");
+		System.out.println("First line is added");
+
 	}
 
 }
